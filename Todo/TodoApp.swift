f950e31d5cba8b03fc5540c55716c23ad1b8d63e
Sidrah Javed <sidrahjaved14@gmail.com>
2023-07-22 12:38:15 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Sidrah Javed on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
